487306a82034bbef5c2a3b6fa925bdc0fe4f9eb7
Raphael Barbosa Rodrigues <42044496+RaphaelBRodrigues@users.noreply.github.com>
2019-08-13 22:19:36 -0300

    Nav currículo

---
+++ js/js.js
@@ -0,0 +1,15 @@
+// $("#brand").focusout(function(){
+// alert("foi");
+// 	$("body").css("background-color", "yellow");
+// });
+
+// function brand(){
+// 	$("body").css("background-color", "yellow");
+
+// $('#inicio').css("color":"white");
+// }
+$(document).ready(function(){
+  $("#brand").focusout(function(){
+  	alert("ola");
+}
+});
