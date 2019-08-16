a91ba9a33152f9b9cc0f09393982972ee4223b7f
Raphael Barbosa Rodrigues <42044496+RaphaelBRodrigues@users.noreply.github.com>
2019-08-15 23:58:57 -0300

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -1,15 +1,32 @@
-// $("#brand").focusout(function(){
-// alert("foi");
-// 	$("body").css("background-color", "yellow");
-// });
-
-// function brand(){
-// 	$("body").css("background-color", "yellow");
-
-// $('#inicio').css("color":"white");
-// }
 $(document).ready(function(){
-  $("#brand").focusout(function(){
-  	alert("ola");
-}
+var hab = document.getElementById("habilidadess");
+var port = document.getElementById("portifolioo");
+var cont = document.getElementById("contatoss");
+
+
+
+
 });
+
+
+function verde(res){
+return res.style.color='green';
+}
+
+function  backcolor(res){
+ return res.style.color='gray';
+
+ }
+function  color(id){
+ 	var el = document.getElementById(id);
+ verde(el);
+  var velha
+ if (velha = el) {
+velha = el;
+ }
+ if (velha != el) {
+ 	backcolor(velha);
+ }
+ alert(id);
+}
+
