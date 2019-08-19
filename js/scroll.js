2b7450ee7a19221e4c6d93793fe20bdf0d4c5b4d
Raphael Barbosa Rodrigues <42044496+RaphaelBRodrigues@users.noreply.github.com>
2019-08-18 22:31:18 -0300

    Scroll
    change nav bg and color items with the scroll

--- js/scroll.js
+++ js/scroll.js
@@ -1,10 +1,62 @@
-//Checa a movimentção do scroll da guia
 $(window).scroll(function(){
-//O Primeiro vê o valor do scroll da guia e o segundo a posição do elemento resumo
-if ($(document).scrollTop() > $("#resumo").position().top) {
-	$("body").css("background-color","white");
-}else{
-		$("body").css("background-color","black");
+if ($(document).scrollTop() < $("#resumo").position().top) {
+	//Inicio
+	var el = document.getElementById("inicio");
+	verde(el);
+		$("#nav").css("background-color","rgba(0,0,0,1)");
 
+}else{
+		backcolor(document.getElementById("inicio"));
+			$("#nav").css("background-color","rgba(0,0,0,0.8)");
+
+
+}
+if ($(document).scrollTop() >= $("#resumo").position().top) {
+	//Resumo
+	if ($(document).scrollTop() < $("#habilidades").position().top) {
+	var el = document.getElementById("resumoo");
+	verde(el);
+
+}else{
+		backcolor(document.getElementById("resumoo"));
+
+}
+}else{
+		backcolor(document.getElementById("resumoo"));
+
+}
+
+if ($(document).scrollTop() >= $("#habilidades").position().top) {
+	//Habilidades
+		if ($(document).scrollTop() < $("#portifolio").position().top) {
+
+	var el = document.getElementById("habilidadess");
+	verde(el);
+}else{
+		backcolor(document.getElementById("habilidadess"));
+
+}
+}else{
+		backcolor(document.getElementById("habilidadess"));
+
+}
+if ($(document).scrollTop() >= $("#portifolio").position().top) {
+	//Portifolio
+		if ($(document).scrollTop() < $("#contatos").position().top) {
+
+	var el = document.getElementById("portifolioo");
+	verde(el);
+}else{
+		backcolor(document.getElementById("portifolioo"));
+
+}
+}else{
+		backcolor(document.getElementById("portifolioo"));
+
+}
+		if ($(document).scrollTop() >= $("#contatos").position().top) {
+
+	var el = document.getElementById("contatoss");
+	verde(el);
 }
 });
