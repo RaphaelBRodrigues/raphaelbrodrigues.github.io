3d231f30a6459007bc361f9932ab87a84d0f6920
Raphael Barbosa Rodrigues <42044496+RaphaelBRodrigues@users.noreply.github.com>
2019-08-20 22:00:49 -0300

    Add files via upload

---
+++ carrosel.js
@@ -0,0 +1,42 @@
+var imgAtual;
+function trocaImg(){
+	//Toda a vez que a função trocaImg é ativa a váriavel la de baixo que antes era 0 ganha mais 1
+	imgAtual++;
+if (imgAtual>2) {
+	//Se a váriavel for maior que o número de imagens(no caso 2) ele retorna para a imagem 1
+	imgAtual = 1;
+
+}
+//Acessa o elemento code e altera o background dele com uma nova imagem que está guardada na pasta
+//Olha a váriavel imgAtual,ela muda o nome do arquivo,antes deveria ser carrosel 0
+//Ai rodou a função e a váriavel ganhou mais um então o nome do arquivo ficou carrosel1.jpg (esse 1 é a váriavel)
+	 $("#code").css("background-image","url('images/carrosel"+imgAtual+".jpg')");
+
+}
+
+
+
+function start(){
+	//A variável é só para definir o nome do arquivo
+imgAtual = 0 ;
+
+//A cada intervalo de 1,3 segundo(1300) roda uma função que roda a função trocaImg()
+setInterval(function(){trocaImg();},1300);
+
+ }
+
+//Quando o documento é aberto "load" ele roda a função "start"S
+ window.addEventListener("load",start);
+
+
+
+// function trocaImg(){
+// //Clica muda a img
+// 	imgAtual++;
+// if (imgAtual>2) {
+// 	imgAtual = 1
+
+// }
+// 	 $("#code").css("background-image","url('images/carrosel"+imgAtual+".jpg')");
+
+// }
---
+++ scroll.js
@@ -0,0 +1,80 @@
+$(window).scroll(function(){
+
+	
+if ($(document).scrollTop() < $("#resumo").position().top) {
+	//Inicio
+	var el = document.getElementById("inicio");
+	verde(el);
+		$("#nav").css("background-color","rgba(0,0,0,1)");
+					// $("#nav").css("position","fixed");
+
+}else{
+		backcolor(document.getElementById("inicio"));
+			$("#nav").css("background-color","rgba(0,0,0,0.8)");
+// 			$("#nav").css("position","absolute");
+// $("#nav").position().top = $("#resumo").position().top;
+
+
+}
+if ($(document).scrollTop() >= $("#resumo").position().top) {
+	//Resumo
+	if ($(document).scrollTop() < $("#habilidades").position().top) {
+	var el = document.getElementById("resumoo");
+	verde(el);
+
+}else{
+		backcolor(document.getElementById("resumoo"));
+
+}
+}else{
+		backcolor(document.getElementById("resumoo"));
+
+}
+
+if ($(document).scrollTop() >= $("#habilidades").position().top) {
+	//Habilidades
+		if ($(document).scrollTop() < $("#portifolio").position().top) {
+
+	var el = document.getElementById("habilidadess");
+	verde(el);
+}else{
+		backcolor(document.getElementById("habilidadess"));
+
+}
+}else{
+		backcolor(document.getElementById("habilidadess"));
+
+}
+if ($(document).scrollTop() >= $("#portifolio").position().top) {
+	//Portifolio
+		if ($(document).scrollTop() < $("#contatos").position().top) {
+
+	var el = document.getElementById("portifolioo");
+	verde(el);
+}else{
+		backcolor(document.getElementById("portifolioo"));
+
+}
+}else{
+		backcolor(document.getElementById("portifolioo"));
+
+}
+		if ($(document).scrollTop() >= $("#contatos").position().top) {
+
+	var el = document.getElementById("contatoss");
+	verde(el);
+}else{
+	backcolor(document.getElementById("contatoss"));
+}
+if($(document).scrollTop() >= $("#portifolio").position().top){
+
+//Fim da guia
+		 $("#container-qr").css("opacity","0.1");
+		 $("#container-qr").css("z-index","-1");
+	}else{
+//Padrao
+		 $("#container-qr").css("opacity","1");
+		 $("#container-qr").css("z-index","991");
+
+	}
+});
