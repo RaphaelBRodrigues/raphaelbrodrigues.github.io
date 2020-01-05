25d5b804ba82281e6a79cbd99015115271c6865f
Raphaelbrodrigues <raphaelbarbosa.rodrigues@gmail.com>
2020-01-05 13:47:26 -0300

    ADD Modals FIX TIme

--- js/load.js
+++ js/load.js
@@ -20,12 +20,19 @@ $(document).ready(function(){
 
 function rel(){
  var dt = new Date();
+ var sec = dt.getSeconds();
  var min = dt.getMinutes();
-
+ var ho  = dt.getHours();
+ if(sec<10){
+	 sec = "0"+sec;
+ }
  if(min<10){
 	  min = "0"+min;
  }
- var hora = dt.getHours() +":"+ min +":"+ dt.getSeconds();
+ if(ho<10){
+	 ho = "0"+ho;
+ }
+ var hora = ho +":"+ min +":"+ sec;
  $("#ho").text(hora);
  $("#nav-clock").val(hora);
 }
